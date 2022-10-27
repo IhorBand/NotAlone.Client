9cc41593304c16b33ae4064057999c88a580cc93
ihorb <ihorb@ipoft.com>
2022-10-27 23:41:53 +0300

    Chat minor fixes

--- src/components/Chat/ChatWindow.tsx
+++ src/components/Chat/ChatWindow.tsx
@@ -3,6 +3,7 @@ import './ChatWindow.css';
 import { MessageModel } from '../../models/MessageModel';
 
 import Message from './Message';
+import { useEffect, useRef } from 'react';
 
 export interface ChatWindowProps {
 	chatMessages: Array<MessageModel>;
@@ -15,8 +16,18 @@ const ChatWindow = (props : ChatWindowProps) => {
             userId={m.userId}
             userName={m.userName}
             message={m.message}/>);
+
+    const messageListDiv = useRef<HTMLDivElement>(null);
+    
+    useEffect(() => {
+        if(messageListDiv && messageListDiv.current) {
+            messageListDiv.current.scrollTop = messageListDiv.current.scrollHeight;
+        }
+    }, 
+    [chat]);
+    
     return(
-        <div className='message-list'>
+        <div ref={messageListDiv} className='message-list'>
             {chat}
         </div>
     )
